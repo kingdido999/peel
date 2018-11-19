b3108f061b8efdffa362e4f58f7a9b2f32aa80f2
kingdido999 <info@desmonding.me>
2018-11-19 16:05:42 +0800

    Get it working

---
+++ index.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const Koa = require('koa')
+const Router = require('koa-router')
+const KoaBody = require('koa-body')
+const views = require('koa-views')
+const util = require('util')
+const readability = require('node-readability')
+const read = util.promisify(readability)
+
+const app = new Koa()
+app.use(views(path.join(__dirname, '/views')))
+app.use(KoaBody())
+
+const router = new Router()
+
+router.get('/', async ctx => {
+  await ctx.render('index')
+})
+
+router.post('/parse', async ctx => {
+  const { url } = ctx.request.body
+  console.log(`URL: ${url}`)
+
+  const article = await read(url)
+  const { title, content } = article
+
+  ctx.response.body = {
+    title,
+    content
+  }
+
+  article.close()
+  console.log('Done.')
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+const PORT = 3333
+app.listen(PORT)
+console.log(`Listening on port: ${PORT}`)
